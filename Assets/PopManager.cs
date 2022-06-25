20617cad5d6f5fd09c8006465f94f881a4453794
blakcen <varelacen@hotmail.com>
2022-06-25 00:12:46 -0400

    ASD

--- Assets/PopManager.cs
+++ Assets/PopManager.cs
@@ -1,10 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class PopManager : MonoBehaviour
 {
     public List<GameObject> pops;
+    private Dictionary<string, UnityEvent> eventdic;
+    private static PopManager popManager;
+    public static PopManager instancia
+    {
+        get
+        {
+            if (!popManager)
+            {
+                popManager = FindObjectOfType(typeof(PopManager)) as PopManager;
+                if (!popManager)
+                {
+                    Debug.LogError("No tenemos CaracolManager");
+                }
+                else
+                {
+                    popManager.Init();
+                }
+            }
+            return popManager;
+        }
+    }
     void Start()
     {
         
@@ -14,5 +35,47 @@ public class PopManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void Init()
+    {
+        if (eventdic == null)
+        {
+            eventdic = new Dictionary<string, UnityEvent>();
+            Debug.Log("Event added");
+        }
+    }
+    public static void StartListening(string eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+        if (instancia.eventdic.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+        else
+        {
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            instancia.eventdic.Add(eventName, thisEvent);
+        }
+    }
+
+    public static void StopLIstening(string eventName, UnityAction listener)
+    {
+        if (instancia == null) return;
+        UnityEvent thisEvent = null;
+        if (instancia.eventdic.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public static void TriggerEvent(string eventName)
+    {
+        UnityEvent thisEvent = null;
+        if (instancia.eventdic.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.Invoke();
+        }
     }
 }
