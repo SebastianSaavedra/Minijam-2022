858ee1e616d2d33ea6aa790ac2a9aad074a397ff
Sebastián Saavedra <sebastiansaavedr@gmail.com>
2022-06-25 01:15:41 -0400

    X moviendose a alguna esquina del Popup

---
+++ Assets/010_Scripts/SebaCode/RandomCrossPosition.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCrossPosition : MonoBehaviour
+{
+    [SerializeField] List<Transform> pivots = new List<Transform>();
+    [SerializeField] float distance = 100f;
+    [SerializeField] float timer = 0;
+
+    void Update()
+    {
+        if (Vector2.Distance(Input.mousePosition, transform.position) < distance)
+        {
+            timer += Time.deltaTime;
+            if (timer >= .4f)
+            {
+                Transform pivot = pivots[Random.Range(0, pivots.Count)];
+                if (pivot.transform.position == transform.position)
+                {
+                    pivot = pivots[Random.Range(0, pivots.Count)];
+                }
+                transform.position = new Vector2(pivot.transform.position.x, pivot.transform.position.y);
+                timer = 0;
+            }
+        }
+    }
+}
