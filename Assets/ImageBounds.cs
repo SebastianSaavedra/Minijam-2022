e6e794ac45bee21c7fb29e44ab34d00a894176bd
blakcen <varelacen@hotmail.com>
2022-06-24 23:29:41 -0400

    Drag

--- Assets/ImageBounds.cs
+++ Assets/ImageBounds.cs
@@ -1,11 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ImageBounds : MonoBehaviour
+public class ImageBounds : MonoBehaviour,IDragHandler,IEndDragHandler,IBeginDragHandler
 {
     public float minX;
     public float maxX;
     public float minY;
     public float maxY;
+    Vector3 _dragOffSet;
+
+
+    Vector3 MousePos()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.z = 0;
+        return mousePos;
+    }
+    public void OnBeginDrag(PointerEventData eventData) 
+    {
+        _dragOffSet = transform.position - MousePos();
+    }
+    public void OnDrag(PointerEventData eventData) 
+    {
+        transform.position = MousePos() +_dragOffSet;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
 }
